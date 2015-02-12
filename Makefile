02f566880d10159715ee23764007bd114dd8a520
notetau <nonotetau@gmail.com>
2015-02-12 20:32:39 +0900

    declare geanycc::python::check_and_install_jediserver_script()

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 
 PLUGIN_NAME := geanyjedicomplete.so
 
-LANG_SRCS := preferences.cpp completion_framework.cpp completion.cpp plugin_info.cpp
+LANG_SRCS := preferences.cpp completion_framework.cpp completion.cpp plugin_info.cpp server.cpp
 LANG_SRCS := $(addprefix src/, $(LANG_SRCS))
 
 CXXFLAGS += -O2
---
+++ src/server.cpp
@@ -0,0 +1,34 @@
+/*
+ * server.cpp - a Geany plugin to provide code completion (Python) using jedi
+ *
+ * Copyright (C) 2015 Noto, Yuta <nonotetau(at)gmail(dot)com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ */
+
+#include "server.hpp"
+
+namespace geanycc
+{
+	namespace python
+	{
+		// TODO
+		bool check_and_install_jediserver_script(const char* path)
+		{
+			return false;
+		}
+	}
+}
